021e88062546f34c52dc4e03f3d345da454cb3a6
Thunder48736 <130589438+Thunder48736@users.noreply.github.com>
2023-06-14 19:34:34 +0200

    converter.py

--- dist/converter.py
+++ dist/converter.py
@@ -1,15 +1,28 @@
-import sys
 import os
 import xmltodict
 import json
 import yaml
+from sys import argv
+
+def read_file(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            return file.read()
+    except IOError:
+        raise IOError(f"Unable to read file: {file_path}")
+
+def write_file(file_path, data):
+    try:
+        with open(file_path, 'w') as file:
+            file.write(data)
+    except IOError:
+        raise IOError(f"Unable to write file: {file_path}")
 
 def convert_data(input_file, output_file):
     _, input_extension = os.path.splitext(input_file)
     _, output_extension = os.path.splitext(output_file)
 
-    with open(input_file, 'r') as file:
-        data = file.read()
+    data = read_file(input_file)
 
     if input_extension == '.xml':
         if output_extension == '.json':
@@ -26,14 +39,19 @@ def convert_data(input_file, output_file):
             converted_data = xmltodict.unparse(yaml.safe_load(data), pretty=True)
         elif output_extension == '.json':
             converted_data = json.dumps(yaml.safe_load(data), indent=4)
+    else:
+        raise ValueError(f"Unsupported file format: {input_extension}")
 
-    with open(output_file, 'w') as file:
-        file.write(converted_data)
+    write_file(output_file, converted_data)
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
+    if len(argv) != 3:
         print('Usage: program.exe input_file output_file')
     else:
-        input_file = sys.argv[1]
-        output_file = sys.argv[2]
-        convert_data(input_file, output_file)
+        input_file = argv[1]
+        output_file = argv[2]
+        try:
+            convert_data(input_file, output_file)
+            print("Conversion successful!")
+        except (IOError, ValueError) as e:
+            print(f"Error: {str(e)}")
